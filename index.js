691694678d30618b94a603f2cb60c13c2cb66041
montyrawal7 <103607972+montyrawal7@users.noreply.github.com>
2022-05-23 18:23:48 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+document.querySelector("button").addEventListener("click", diceRoll);
+
+
+function diceRoll() {
+  var randomNo= Math.floor(Math.random() * 6) + 1 ;
+  var randomImage= "dice" + randomNo + ".png";
+  var randomImageSource= document.querySelectorAll("img") [0];
+  randomImageSource.setAttribute("src", randomImage);
+
+  var randomNo1= Math.floor(Math.random() * 6) + 1 ;
+  var randomImage1= "dice" + randomNo1 + ".png";
+  var randomImageSource1= document.querySelectorAll("img") [1];
+  randomImageSource1.setAttribute("src", randomImage1);
+
+  if (randomNo > randomNo1) {
+    document.querySelector("h1").innerHTML= "You won!🥳";
+  } else if (randomNo < randomNo1) {
+    document.querySelector("h1").innerHTML= "Oops, the computer!☹️";
+  } else {
+    document.querySelector("h1").innerHTML= "It's a draw! Try again.😊";
+  }
+
+}
